831e28b150c29acbeba0ae403c685029e604b656
przemeksw1 <przemek_sw1@wp.pl>
2019-01-26 14:58:18 +0100

    Api

---
+++ EventR_Backend/EventR/Controllers/GrafikisController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EventRApi.Models;
+
+namespace EventR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GrafikisController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public GrafikisController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Grafikis
+        [HttpGet]
+        public IEnumerable<Grafiki> Getgrafiki()
+        {
+            return _context.grafiki;
+        }
+
+        // GET: api/Grafikis/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetGrafiki([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var grafiki = await _context.grafiki.FindAsync(id);
+
+            if (grafiki == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(grafiki);
+        }
+
+        // PUT: api/Grafikis/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGrafiki([FromRoute] int id, [FromBody] Grafiki grafiki)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != grafiki.Id_Grafiki)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(grafiki).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GrafikiExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Grafikis
+        [HttpPost]
+        public async Task<IActionResult> PostGrafiki([FromBody] Grafiki grafiki)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.grafiki.Add(grafiki);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGrafiki", new { id = grafiki.Id_Grafiki }, grafiki);
+        }
+
+        // DELETE: api/Grafikis/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGrafiki([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var grafiki = await _context.grafiki.FindAsync(id);
+            if (grafiki == null)
+            {
+                return NotFound();
+            }
+
+            _context.grafiki.Remove(grafiki);
+            await _context.SaveChangesAsync();
+
+            return Ok(grafiki);
+        }
+
+        private bool GrafikiExists(int id)
+        {
+            return _context.grafiki.Any(e => e.Id_Grafiki == id);
+        }
+    }
+}
--- EventR_Backend/EventR/Startup.cs
+++ EventR_Backend/EventR/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EventRApi.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,7 +27,9 @@ namespace EventR
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<Context>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +44,11 @@ namespace EventR
                 app.UseHsts();
             }
 
+                  app.UseCors(builder =>
+             builder
+             .AllowAnyMethod()
+             .AllowAnyHeader()
+             .AllowAnyOrigin());
             app.UseHttpsRedirection();
             app.UseMvc();
         }
